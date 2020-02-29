c281868339c97ee41fe5e6709d8d454116482da4
Phu Vuong <pvuong@vuonglab.com>
2020-02-28 19:01:39 -0700

    Update duration labels when app becomes active.

--- app/src/main/java/com/vuonglab/hennaandphu/MainActivity.java
+++ app/src/main/java/com/vuonglab/hennaandphu/MainActivity.java
@@ -100,120 +100,139 @@ public class MainActivity extends AppCompatActivity {
             if (marriedDuration.Years != previousMarriedDuration.Years) {
                 yearsCount.setText(String.valueOf(marriedDuration.Years));
 
+                long expectedYears = previousMarriedDuration.Years + 1;
+                boolean updateSkipped = (expectedYears != marriedDuration.Years);
+
                 if (marriedDuration.Years == 0 || marriedDuration.Years == 1)
                     yearsLabel.setText(R.string.year);
-                else if (marriedDuration.Years == 2)
+                else if (marriedDuration.Years == 2 || updateSkipped)
                     yearsLabel.setText(R.string.years);
 
                 if (marriedDuration.Years == 0) {
                     yearsCount.setEnabled(false);
                     yearsLabel.setEnabled(false);
-                } else if (marriedDuration.Years == 1) {
+                } else if (marriedDuration.Years == 1 || updateSkipped) {
                     yearsCount.setEnabled(true);
                     yearsLabel.setEnabled(true);
                 }
 
-                Log.d(TAG, "Years updated");
+                Log.d(TAG, "Years updated" + (updateSkipped ? " (skipped)" : ""));
             }
 
             if (marriedDuration.Months != previousMarriedDuration.Months) {
                 monthsCount.setText(String.valueOf(marriedDuration.Months));
 
+                long expectedMonths = (previousMarriedDuration.Months == 11 ? 0 : previousMarriedDuration.Months+1);
+                boolean updateSkipped = (expectedMonths != marriedDuration.Months);
+
                 if (marriedDuration.Months == 0 || marriedDuration.Months == 1)
                     monthsLabel.setText(R.string.month);
-                else if (marriedDuration.Months == 2)
-                        monthsLabel.setText(R.string.months);
+                else if (marriedDuration.Months == 2 || updateSkipped)
+                    monthsLabel.setText(R.string.months);
 
                 if (marriedDuration.Months == 0) {
                     monthsCount.setEnabled(false);
                     monthsLabel.setEnabled(false);
-                } else if (marriedDuration.Months == 1) {
+                } else if (marriedDuration.Months == 1 || updateSkipped) {
                     monthsCount.setEnabled(true);
                     monthsLabel.setEnabled(true);
                 }
 
-                Log.d(TAG, "Months updated");
+                Log.d(TAG, "Months updated" + (updateSkipped ? " (skipped)" : ""));
             }
 
             if (marriedDuration.Days != previousMarriedDuration.Days) {
                 daysCount.setText(String.valueOf(marriedDuration.Days));
 
+                boolean updateSkipped = true;
+                if (previousMarriedDuration.Days <= 26) {
+                    long expectedDays = previousMarriedDuration.Days + 1;
+                    updateSkipped = (expectedDays != marriedDuration.Days);
+                }
+
                 if (marriedDuration.Days == 0 || marriedDuration.Days == 1)
                     daysLabel.setText(R.string.day);
-                else if (marriedDuration.Days == 2)
+                else if (marriedDuration.Days == 2 || updateSkipped)
                     daysLabel.setText(R.string.days);
 
                 if (marriedDuration.Days == 0) {
                     daysCount.setEnabled(false);
                     daysLabel.setEnabled(false);
-                } else if (marriedDuration.Days == 1) {
+                } else if (marriedDuration.Days == 1 || updateSkipped) {
                     daysCount.setEnabled(true);
                     daysLabel.setEnabled(true);
                 }
 
-                Log.d(TAG, "Days updated");
+                Log.d(TAG, "Days updated" + (updateSkipped ? " (skipped)" : ""));
             }
 
             if (marriedDuration.Hours != previousMarriedDuration.Hours) {
                 hoursCount.setText(String.valueOf(marriedDuration.Hours));
 
+                long expectedHours = (previousMarriedDuration.Hours == 23 ? 0 : previousMarriedDuration.Hours+1);
+                boolean updateSkipped = (expectedHours != marriedDuration.Hours);
+
                 if (marriedDuration.Hours == 0 || marriedDuration.Hours == 1)
                     hoursLabel.setText(R.string.hour);
-                else if (marriedDuration.Hours == 2)
+                else if (marriedDuration.Hours == 2 || updateSkipped)
                     hoursLabel.setText(R.string.hours);
 
                 if (marriedDuration.Hours == 0) {
                     hoursCount.setEnabled(false);
                     hoursLabel.setEnabled(false);
-                } else if (marriedDuration.Hours == 1) {
+                } else if (marriedDuration.Hours == 1 || updateSkipped) {
                     hoursCount.setEnabled(true);
                     hoursLabel.setEnabled(true);
                 }
 
-                Log.d(TAG, "Hours updated");
+                Log.d(TAG, "Hours updated" + (updateSkipped ? " (skipped)" : ""));
             }
 
             if (marriedDuration.Minutes != previousMarriedDuration.Minutes) {
                 minutesCount.setText(String.valueOf(marriedDuration.Minutes));
 
+                long expectedMinutes = (previousMarriedDuration.Minutes == 59 ? 0 : previousMarriedDuration.Minutes+1);
+                boolean updateSkipped = (expectedMinutes != marriedDuration.Minutes);
+
                 if (marriedDuration.Minutes == 0 || marriedDuration.Minutes == 1)
                     minutesLabel.setText(R.string.minute);
-                else if (marriedDuration.Minutes == 2)
+                else if (marriedDuration.Minutes == 2 || updateSkipped)
                     minutesLabel.setText(R.string.minutes);
 
                 if (marriedDuration.Minutes == 0) {
                     minutesCount.setEnabled(false);
                     minutesLabel.setEnabled(false);
-                } else if (marriedDuration.Minutes == 1) {
+                } else if (marriedDuration.Minutes == 1 || updateSkipped) {
                     minutesCount.setEnabled(true);
                     minutesLabel.setEnabled(true);
                 }
 
-                Log.d(TAG, "Minutes updated");
+                Log.d(TAG, "Minutes updated" + (updateSkipped ? " (skipped)" : ""));
             }
 
             if (marriedDuration.Seconds != previousMarriedDuration.Seconds) {
                 secondsCount.setText(String.valueOf(marriedDuration.Seconds));
 
+                long expectedSeconds = (previousMarriedDuration.Seconds == 59 ? 0 : previousMarriedDuration.Seconds+1);
+                boolean updateSkipped = (expectedSeconds != marriedDuration.Seconds);
+
                 if (marriedDuration.Seconds == 0 || marriedDuration.Seconds == 1)
                     secondsLabel.setText(R.string.second);
-                else if (marriedDuration.Seconds == 2)
+                else if (marriedDuration.Seconds == 2 || updateSkipped)
                     secondsLabel.setText(R.string.seconds);
 
                 if (marriedDuration.Seconds == 0) {
                     secondsCount.setEnabled(false);
                     secondsLabel.setEnabled(false);
-                } else if (marriedDuration.Seconds == 1) {
+                } else if (marriedDuration.Seconds == 1 || updateSkipped) {
                     secondsCount.setEnabled(true);
                     secondsLabel.setEnabled(true);
                 }
 
-                Log.d(TAG, "Seconds updated");
+                Log.d(TAG, "Seconds updated" + (updateSkipped ? " (skipped)" : ""));
             }
 
             previousMarriedDuration = marriedDuration;
-
-            Log.d(TAG, "Timer running...");
         }
     };
 
