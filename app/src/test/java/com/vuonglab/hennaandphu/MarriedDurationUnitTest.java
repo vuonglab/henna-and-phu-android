2548a8796f72cdb35c4ed59d2c8d9e945bec20e8
Phu Vuong <pvuong@vuonglab.com>
2020-03-29 12:18:36 -0700

    Add first unit test for duration calculation.

--- app/src/test/java/com/vuonglab/hennaandphu/MarriedDurationUnitTest.java
+++ app/src/test/java/com/vuonglab/hennaandphu/MarriedDurationUnitTest.java
@@ -3,9 +3,30 @@ package com.vuonglab.hennaandphu;
 import static com.google.common.truth.Truth.assertThat;
 import org.junit.Test;
 
+import java.time.ZoneId;
+import java.time.ZonedDateTime;
+
 public class MarriedDurationUnitTest {
     @Test
-    public void addition_isCorrect() {
-        assertThat(2 + 2).isEqualTo(4);
+    public void zero_duration() {
+        ZonedDateTime now = getPhoenixDateTime(2015, 3, 14, 9, 26, 53);
+        Duration marriedDuration = DurationCalculator.getMarriedDuration(now);
+        assertDuration(marriedDuration, new Duration(0, 0, 0, 0, 0, 0));
+    }
+
+    private ZonedDateTime getPhoenixDateTime(int year, int month, int dayOfMonth, int hour, int minute, int second)
+    {
+        return ZonedDateTime.of(year, month, dayOfMonth, hour, minute, second, 0, ZoneId.of("America/Phoenix"));
+    }
+
+    private void assertDuration(Duration actualDuration, Duration expectedDuration)
+    {
+        assertThat(actualDuration.Years).isEqualTo(expectedDuration.Years);
+        assertThat(actualDuration.Months).isEqualTo(expectedDuration.Months);
+        assertThat(actualDuration.Days).isEqualTo(expectedDuration.Days);
+
+        assertThat(actualDuration.Hours).isEqualTo(expectedDuration.Hours);
+        assertThat(actualDuration.Minutes).isEqualTo(expectedDuration.Minutes);
+        assertThat(actualDuration.Seconds).isEqualTo(expectedDuration.Seconds);
     }
 }
