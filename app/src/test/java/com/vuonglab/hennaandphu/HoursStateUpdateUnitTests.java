0187d790b85591730b694d5542ea563d42a84e30
Phu Vuong <pvuong@vuonglab.com>
2020-05-05 23:20:11 -0700

    Unit test checks for enabling or disabling the hours field and label.

---
+++ app/src/test/java/com/vuonglab/hennaandphu/HoursStateUpdateUnitTests.java
@@ -0,0 +1,190 @@
+package com.vuonglab.hennaandphu;
+
+import com.vuonglab.hennaandphu.UIUpdateOptimizations.StateUpdate;
+
+import org.junit.Test;
+
+import static com.google.common.truth.Truth.assertThat;
+import static com.vuonglab.hennaandphu.UIUpdateOptimizations.GetHoursStateUpdate;
+
+public class HoursStateUpdateUnitTests {
+    @Test
+    public void appStartZeroHour() {
+        runTest(-1, 0, StateUpdate.SHOW_DISABLED);
+    }
+
+    @Test
+    public void appStartOneHour() {
+        runTest(-1, 1, StateUpdate.SHOW_ENABLED);
+    }
+
+    @Test
+    public void appStartTwoHours() {
+        runTest(-1, 2, StateUpdate.SHOW_ENABLED);
+    }
+
+    @Test
+    public void appStartTwentyThreeHours() {
+        runTest(-1, 23, StateUpdate.SHOW_ENABLED);
+    }
+
+    @Test
+    public void zeroToOneHour() {
+        runTest(0, 1, StateUpdate.SHOW_ENABLED);
+    }
+
+    @Test
+    public void zeroToTwoHours() {
+        runTest(0, 2, StateUpdate.SHOW_ENABLED);
+    }
+
+    @Test
+    public void zeroToTwentyThreeHours() {
+        runTest(0, 23, StateUpdate.SHOW_ENABLED);
+    }
+
+    @Test
+    public void zeroToZeroHour() {
+        runTest(0, 0, StateUpdate.NOT_NEEDED);
+    }
+
+    @Test
+    public void oneToTwoHours() {
+        runTest(1, 2, StateUpdate.NOT_NEEDED);
+    }
+
+    @Test
+    public void oneToTwentyThreeHours() {
+        runTest(1, 23, StateUpdate.NOT_NEEDED);
+    }
+
+    @Test
+    public void oneToZeroHour() {
+        runTest(1, 0, StateUpdate.SHOW_DISABLED);
+    }
+
+    @Test
+    public void oneToOneHour() {
+        runTest(1, 1, StateUpdate.NOT_NEEDED);
+    }
+
+    @Test
+    public void twoToThreeHours() {
+        runTest(2, 3, StateUpdate.NOT_NEEDED);
+    }
+
+    @Test
+    public void twoToSeventeenHours() {
+        runTest(2, 17, StateUpdate.NOT_NEEDED);
+    }
+
+    @Test
+    public void twoToTwentyThreeHours() {
+        runTest(2, 23, StateUpdate.NOT_NEEDED);
+    }
+
+    @Test
+    public void twoToZeroHour() {
+        runTest(2, 0, StateUpdate.SHOW_DISABLED);
+    }
+
+    @Test
+    public void twoToOneHour() {
+        runTest(2, 1, StateUpdate.NOT_NEEDED);
+    }
+
+    @Test
+    public void twoToTwoHours() {
+        runTest(2, 2, StateUpdate.NOT_NEEDED);
+    }
+
+    @Test
+    public void threeToFourHours() {
+        runTest(3, 4, StateUpdate.NOT_NEEDED);
+    }
+
+    @Test
+    public void threeToFourteenHours() {
+        runTest(3, 14, StateUpdate.NOT_NEEDED);
+    }
+
+    @Test
+    public void threeToTwentyThreeHours() {
+        runTest(3, 23, StateUpdate.NOT_NEEDED);
+    }
+
+    @Test
+    public void threeToZeroHour() {
+        runTest(3, 0, StateUpdate.SHOW_DISABLED);
+    }
+
+    @Test
+    public void threeToOneHour() {
+        runTest(3, 1, StateUpdate.NOT_NEEDED);
+    }
+
+    @Test
+    public void threeToTwoHours() {
+        runTest(3, 2, StateUpdate.NOT_NEEDED);
+    }
+
+    @Test
+    public void threeToThreeHours() {
+        runTest(3, 3, StateUpdate.NOT_NEEDED);
+    }
+
+    @Test
+    public void sixteenToTwentyThreeHours() {
+        runTest(16, 23, StateUpdate.NOT_NEEDED);
+    }
+
+    @Test
+    public void sixteenToZeroHour() {
+        runTest(16, 0, StateUpdate.SHOW_DISABLED);
+    }
+
+    @Test
+    public void sixteenToOneHour() {
+        runTest(16, 1, StateUpdate.NOT_NEEDED);
+    }
+
+    @Test
+    public void sixteenToTwoHours() {
+        runTest(16, 2, StateUpdate.NOT_NEEDED);
+    }
+
+    @Test
+    public void twentyThreeToZeroHour() {
+        runTest(23, 0, StateUpdate.SHOW_DISABLED);
+    }
+
+    @Test
+    public void twentyThreeToOneHour() {
+        runTest(23, 1, StateUpdate.NOT_NEEDED);
+    }
+
+    @Test
+    public void twentyThreeToTwoHours() {
+        runTest(23, 2, StateUpdate.NOT_NEEDED);
+    }
+
+    @Test
+    public void twentyThreeToTenHours() {
+        runTest(23, 10, StateUpdate.NOT_NEEDED);
+    }
+
+    @Test
+    public void twentyThreeToTwentyHours() {
+        runTest(23, 20, StateUpdate.NOT_NEEDED);
+    }
+
+    @Test
+    public void twentyThreeToTwentyThreeHours() {
+        runTest(23, 23, StateUpdate.NOT_NEEDED);
+    }
+
+    private void runTest(long previousHoursDuration, long currentHoursDuration, StateUpdate expectedStateUpdate) {
+        StateUpdate stateUpdate = GetHoursStateUpdate(currentHoursDuration, previousHoursDuration);
+        assertThat(stateUpdate).isEqualTo(expectedStateUpdate);
+    }
+}
